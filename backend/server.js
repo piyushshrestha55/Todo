127a1c707e479401ca3cbcb9e5a1db46e730f7d8
piyush55 <snatch.sth@gmail.com>
2026-01-06 12:23:31 +0545

    Todo app completed

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,6 @@ app.get("/sendData", async (req, res) => {
 });
 
 app.post("/todos", async (req, res) => {
-  console.log(req.body);
   try {
     let todo = new Todo(req.body); //req.body === to new document todo passed from frontend as newTodo
     await todo.save(); //the todo is saved as document in todoDB
@@ -35,6 +34,52 @@ app.post("/todos", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+app.put("/update/:id", async (req, res) => {
+  console.log(req.body);
+  let todo = req.body;
+  try {
+    const result = await Todo.updateOne(
+      { id: req.params.id },
+      { $set: { des: todo.des } }
+    );
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: "Todo not found or unchanged" });
+    }
+    res.json({ message: "Todo updated" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/delete/:id", async (req, res) => {
+  try {
+    const result = await Todo.deleteOne({ id: req.params.id });
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: "Todo not found or unchanged" });
+    }
+    res.json({ message: "Todo deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.patch("/updateCheck/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findOne({ id: req.params.id });
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    todo.isCompleted = !todo.isCompleted;
+    await todo.save();
+
+    res.json({ message: "Todo updated", todo });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`The app is listening at the port ${port}`);
 });
--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+  const [showFinished, setShowFinished] = useState(true);
   const {
     register,
     handleSubmit,
@@ -54,10 +58,68 @@ export default function App() {
     await saveToDB(newTodo);
   };
 
+  const handleCheckbox = async (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((i) => {
+      return i.id === id;
+    });
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+    try {
+      const response = await fetch(`http://localhost:3000/updateCheck/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" }
+      });
+      const result = await response.json();
+      console.log("Yes", result.message);
+    } catch (err) {
+      console.log("Error updating Todo ", err);
+    }
+  };
+
+  const handleUpdate = async (id, newDes) => {
+    let newTodo = newDes;
+    try {
+      const response = await fetch(`http://localhost:3000/update/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ des: newDes })
+      });
+      const result = await response.json();
+      console.log("Yes, ", result.message);
+      setTodos(
+        todos.map((todo) => (todo.id === id ? { ...todo, des: newDes } : todo))
+      );
+      setEditingId(null);
+    } catch (err) {
+      console.error("Error updating todo:", err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      let newTodos = todos.filter((i) => {
+        return i.id !== id;
+      });
+      setTodos(newTodos);
+      const response = await fetch(`http://localhost:3000/delete/${id}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      });
+      const result = await response.json();
+      console.log("Yes, ", result.message);
+    } catch (err) {
+      console.error("Failed to delete todo", err);
+    }
+  };
+
   return (
     <>
       <NavBar />
-      <div className="container w-[50%] mx-auto h-[90vh] bg-violet-200 my-3 p-5 rounded-2xl">
+      <div className="mx-3 container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
         <div className="inputs">
           <h1 className="font-bold text-2xl mx-[45%] my-3">iTasks</h1>
           <form className="flex gap-2" onSubmit={handleSubmit(onSubmit)}>
@@ -73,22 +135,67 @@ export default function App() {
             )}
             <button
               type="submit"
-              className="bg-violet-500 text-white px-4 py-1 rounded cursor-pointer"
+              className="bg-violet-900 text-white px-4 py-1 rounded cursor-pointer"
             >
               Save
             </button>
           </form>
         </div>
         <div className="todos">
+          <input
+            type="checkbox"
+            name=""
+            checked={showFinished}
+            onChange={() => setShowFinished(!showFinished)}
+          />
+          Show Finished
+          {todos.length == 0 && (
+            <div className="font-xl italic m-5">No Todos to show </div>
+          )}
           {todos.map((item) => {
             return (
-              <>
-                <div key={item._id}>
-                  {" "}
-                  <span>{item.des}</span>
-                  <span>{item.isCompleted ? "✅" : "❌"}</span>
+              (showFinished || !item.isCompleted) && (
+                <div key={item.id} className="flex justify-between mx-1 my-2">
+                  <div className="flex gap-2 items-center">
+                    <input
+                      type="checkbox"
+                      onChange={handleCheckbox}
+                      name={item.id}
+                      checked={item.isCompleted}
+                    />
+                    <div className={item.isCompleted ? "line-through" : ""}>
+                      {editingId === item.id ? (
+                        <input
+                          type="text"
+                          defaultValue={item.des}
+                          onBlur={(e) => handleUpdate(item.id, e.target.value)}
+                          autoFocus
+                        />
+                      ) : (
+                        <span>{item.des}</span>
+                      )}
+                    </div>
+                  </div>
+                  <div className="buttons flex h-full ">
+                    <button
+                      className="bg-violet-900 cursor-pointer px-3 py-1 mx-2 text-sm font-bold rounded-md hover:bg-violet-950 text-white"
+                      onClick={() => {
+                        setEditingId(item.id);
+                      }}
+                    >
+                      <FaEdit />
+                    </button>
+                    <button
+                      className="bg-violet-900 cursor-pointer px-3 py-1 mx-2 text-sm font-bold rounded-md hover:bg-violet-950 text-white"
+                      onClick={(e) => {
+                        handleDelete(item.id);
+                      }}
+                    >
+                      <AiFillDelete />
+                    </button>
+                  </div>
                 </div>
-              </>
+              )
             );
           })}
         </div>
